262bfe59ff8739fd7b0d96f7e66948d57822e037
Nazerke <abdumaliknazerke9@gmail.com>
2026-01-21 17:27:26 +0500

    init

---
+++ src/main/java/org/qazcodenarxoz/mainmicroservice1/MainMicroservice1Application.java
@@ -0,0 +1,13 @@
+package org.qazcodenarxoz.mainmicroservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainMicroservice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainMicroservice1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/qazcodenarxoz/mainmicroservice1/MainMicroservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qazcodenarxoz.mainmicroservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainMicroservice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
