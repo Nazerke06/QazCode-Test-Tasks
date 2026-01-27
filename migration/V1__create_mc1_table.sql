41426b038e5d56c38713d995aec35f17c4215b01
Abdimalik Nazerke <151700719+Nazerke06@users.noreply.github.com>
2026-01-27 15:57:22 +0500

    Delete migration/V1__create_mc1_table.sql

--- migration/V1__create_mc1_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE mc1_messages (
-                              id INT PRIMARY KEY AUTO_INCREMENT,
-                              session_id INT NOT NULL,
-                              MC1_timestamp TIMESTAMP WITH TIME ZONE,
-                              MC2_timestamp DATETIME,
-                              MC3_timestamp DATETIME,
-                              end_timestamp DATETIME
-);
